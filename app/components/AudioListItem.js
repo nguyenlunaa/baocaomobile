9b14ddb8425e128bb0fd4c1e7911acae89a19efc
nguyenlunaa <nguyen12072kk2@gmail.com>
2022-10-10 09:52:11 +0700

    baocaomobile

---
+++ app/components/AudioListItem.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AudioListItem = () => {
+  return (
+    <View>
+      <Text>AudioListItem</Text>
+    </View>
+  )
+}
+
+export default AudioListItem
+
+const styles = StyleSheet.create({})
---
+++ app/screens/AudioList.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet,ScrollView, Dimensions} from 'react-native'
+import { AudioContext } from '../context/AudioProvider'
+import {RecyclerListView, LayoutProvider} from 'recyclerlistview'
+export class AudioList extends Component {
+  static contextType = AudioContext;
+
+  layoutProvider = new LayoutProvider((i) => 'audio', (type, dim) => {
+    switch(type) {
+      case 'audio': 
+      dim.width = Dimensions.get('window').width;
+      dim.height = 70;
+      break;
+      default:
+      dim.width = 0;
+      dim.height = 0;  
+    }
+  })
+
+  rowRenderer = (type, item) => {
+    console.log(item);
+    return<Text>{item.filename}</Text>
+  }
+
+  render() {
+    return <AudioContext.Consumer>
+      {({dataProvider}) => {
+        return (
+          <View style= {{flex: 1}}>
+
+          
+          <RecyclerListView dataProvider={dataProvider} layoutProvider= {this.layoutProvider} rowRenderer={this.rowRenderer} />
+          </View>
+        );
+      }}
+    </AudioContext.Consumer>
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+})
+
+export default AudioList
